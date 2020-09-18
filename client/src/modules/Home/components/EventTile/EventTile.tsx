34522d661e34474e99439014bce41f9d9ab44073
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-09-18 20:58:29 +0530

    feat(client): create event tile

---
+++ client/src/modules/Home/components/EventTile/EventTile.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import "./EventTile.scss";
+import { EventProps } from "../EventRow/EventRow";
+
+const EventTile = ({ title, thumbnail_url, description, tags }: EventProps) => {
+  return (
+    <div className="ev-event-tile">
+      <figure>
+        <img src={thumbnail_url} alt="Kitten" />
+      </figure>
+      <div>
+        <div className="content">
+          <h4>{title}</h4>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventTile;
