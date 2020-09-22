9d1685c8eb605f672a2c8f78d8d17d92ce7cde6b
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-09-22 12:20:15 +0530

    feat(server/qrcode): add db schema

--- api/events/events.schema.ts
+++ api/events/events.schema.ts
@@ -10,12 +10,20 @@ export interface EventSchema {
     fileType: string;
     fontColor: "WHITE" | "BLACK";
     objects: Array<{
-      type: "name" | "position" | "theme";
+      type: "name" | "winner" | "theme";
       x: number;
       y: number;
       maxWidth: number;
       maxHeight: number;
       fontSize: 8 | 16 | 32 | 64 | 128;
     }>;
+    qr?: {
+      enabled: boolean;
+      darkHex: string;
+      lightHex: string;
+      x: number;
+      y: number;
+      size: number;
+    };
   };
 }
