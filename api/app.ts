853949c19b7c2a98ad5458049fe3364784c0d73f
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-10-09 03:21:57 +0530

    fix(server): helmet config

--- api/app.ts
+++ api/app.ts
@@ -5,12 +5,14 @@ import bodyParser from "body-parser";
 import { config as dotenvConfig } from "dotenv";
 import httpLogger from "pino-http";
 
-import { errorHandler } from "./error/error.handler";
+import { errorHandler, ApiError } from "./error/error.handler";
 import authRoutes from "./auth/auth.routes";
+import eventsRoutes from "./events/events.routes";
 import certificateRoutes from "./certificates/certificates.routes";
 import { DatabaseService } from "./services/database.service";
 import { LoggerService } from "./services/logger.service";
 import { StorageService } from "./services/storage.service";
+import { errors } from "./error/error.constant";
 
 dotenvConfig();
 const app: Express = express();
@@ -21,17 +23,47 @@ const corsOptions: CorsOptions = {
     if (whitelist.indexOf(origin!) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS."));
+      callback({ ...errors.CORS_ERROR, name: "CORS_ERROR" } as ApiError);
     }
   },
 };
 if (process.env.NODE_ENV === "production") {
   app.use(cors(corsOptions));
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "https://fonts.googleapis.com",
+            "https://fonts.gstatic.com",
+          ],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          styleSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "https://fonts.googleapis.com",
+            "https://fonts.gstatic.com",
+          ],
+          fontSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            "https://fonts.googleapis.com",
+            "https://fonts.gstatic.com",
+          ],
+          imgSrc: [
+            "'self'",
+            "data: blob: https://aaruush20-assets.s3.ap-south-1.amazonaws.com",
+          ],
+        },
+      },
+    })
+  );
 } else {
   app.use(cors());
 }
 
-app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -39,6 +71,7 @@ app.use(httpLogger({ redact: ["req.headers.authorization"] }));
 
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", certificateRoutes);
+app.use("/api/v1", eventsRoutes);
 
 app.use(errorHandler);
 
@@ -56,7 +89,7 @@ Promise.all([
   .then(() => {
     app.listen(process.env.PORT, () => {
       LoggerService.getInstance().log.info(
-        `Listening For Awesome Stuff on Port ${process.env.PORT}`
+        `Server:${process.env.NODE_ENV}-mode on Port ${process.env.PORT}`
       );
     });
   })
