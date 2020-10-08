60ac95b9d40abad5c4b2b45c01139ce10823e526
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-10-09 03:23:30 +0530

    feat(server/events): add schema

--- api/events/events.schema.ts
+++ api/events/events.schema.ts
@@ -1,3 +1,4 @@
+import * as yup from "yup";
 export interface certSchema {
   category: string;
   fileName: string;
@@ -21,12 +22,54 @@ export interface certSchema {
 }
 
 export interface EventSchema {
-  _id: string;
+  _id?: string;
   name: string;
   slug: string;
   date: Date;
-  poster: string;
+  poster: Array<string>;
   description: string;
+  category: string;
   link: string;
   certificates: Array<certSchema>;
+}
+
+export const EventPostRequestSchema = yup
+  .object({
+    name: yup.string().trim().min(1, "name cannot be null").required(),
+    date: yup
+      .date()
+      .min(new Date("01 January 2020 00:00:00 +5:30"), "invalid date")
+      .required(),
+    poster: yup
+      .array(
+        yup
+          .string()
+          .trim()
+          .min(1, "poster[i] cannot be null")
+          .url("poster[i] not a valid URL")
+          .required()
+      )
+      .min(1)
+      .required(),
+    link: yup.string().trim().min(1, "link cannot be null").required(),
+    description: yup
+      .string()
+      .trim()
+      .min(1, "description cannot be null")
+      .required(),
+    category: yup
+      .string()
+      .trim()
+      .min(1, "description cannot be null")
+      .required(),
+  })
+  .required();
+
+export interface EventPostRequest
+  extends Omit<
+    Omit<yup.InferType<typeof EventPostRequestSchema>, "poster">,
+    "date"
+  > {
+  poster: Array<string>;
+  date: Date;
 }
